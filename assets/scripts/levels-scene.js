8aa62732d795dfd359ed9970f392a3dbb3586d19
Andrey Feldman <andreyf@ITMIKI-MAC.local>
2017-06-08 19:04:30 +0300

    Levels

--- assets/scripts/levels-scene.js
+++ assets/scripts/levels-scene.js
@@ -2,7 +2,7 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        lebelButtonPrefab: {
+        levelButtonPrefab: {
             default: null,
             type: cc.Prefab
         }
@@ -10,20 +10,36 @@ cc.Class({
 
     // use this for initialization
     onLoad: function () {
-        var lebels = [
-            {
-                locked: false,
-                position: cc.p(0, 0)
-            },
-            {
-                locked: true,
-                position: cc.p(0, 100)
-            },
-            {
-                locked: true,
-                position: cc.p(0, 200)
-            },
-        ];
+        // var lebels = [
+        //     {
+        //         locked: false,
+        //         position: cc.p(0, 0)
+        //     },
+        //     {
+        //         locked: true,
+        //         position: cc.p(0, 100)
+        //     },
+        //     {
+        //         locked: true,
+        //         position: cc.p(0, 200)
+        //     },
+        // ];
+        
+        var newLevelButton = cc.instantiate(this.levelButtonPrefab);
+        var button = newLevelButton.getComponent(cc.Button);
+        var label = newLevelButton.getComponent('level-button').label;
+        label.string = '10';
+        // newLevelButton.getChildByName('Label').string = '1';
+        console.log('--------'+label.string);
+        // var prop;
+        // for (prop in newLevelButton) {
+        //     console.log(prop);
+        // }
+
+        // this.node.addChild(newLevelButton);
+        // newLevelButton._disabled = true;
+        newLevelButton.parent = this.node;
+        newLevelButton.setPosition(cc.p(0, 100));
     },
 
     // called every frame, uncomment this function to activate update callback

1bdc9625c0d39e96035fa56167c65817893a5cf1
Andrey Feldman <andreyf@ITMIKI-MAC.local>
2017-08-03 18:47:10 +0300

    high scores

--- assets/levels/scores-container.js
+++ assets/levels/scores-container.js
@@ -4,13 +4,31 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+        audioMng: cc.Node,
+
         myScore: cc.Label,
-        bestWeekScore: cc.Label
+        highScoresBtn: cc.Button,
+
+        modalHighScores: {
+            default: null,
+            type: cc.Node
+        }
     },
 
     // use this for initialization
     onLoad: function () {
+        this.audioMng = this.audioMng.getComponent('AudioMng');
+
         this.myScore.string = flow.getMyScore();
+    },
+    
+    highScores: function() {
+        this.audioMng.playButton();
+        this.modalHighScores.getComponent('ModalUI').show();
+     },
+    
+    closeHighScores: function() {
+        this.modalHighScores.getComponent('ModalUI').hide();
     },
 
     // called every frame, uncomment this function to activate update callback
--- assets/scripts/levels-scene.js
+++ assets/scripts/levels-scene.js
@@ -24,6 +24,7 @@ cc.Class({
             default: null,
             type: cc.Prefab
         }
+
     },
 
     // use this for initialization
